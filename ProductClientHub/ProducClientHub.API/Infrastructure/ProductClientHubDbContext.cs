fbf80f5d38a54b699437aab25d10ee93115dd6cb
Joao Vitor Resende <54758120+JoaoVitorResende@users.noreply.github.com>
2025-02-02 11:37:55 -0300

    Inicio de configuração de infrastructure "banco de dados"

---
+++ ProductClientHub/ProducClientHub.API/Infrastructure/ProductClientHubDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProducClientHub.API.Entities;
+
+namespace ProducClientHub.API.Infrastructure
+{
+    public class ProductClientHubDbContext : DbContext
+    {
+        public DbSet<Client> Clients {  get; set; }
+        public DbSet<Product> Products {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source =E:\\Git\\ProductClientHub");
+        }
+    }
+}
